1f70432b607c21f86bb6ab10ebe8abb6efe91030
Muhammet KESKİN <muhammet.keskin@openmindsoft.com>
2024-10-29 22:30:16 +0300

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using MyPortfolioMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolioMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        MyPortfolio _db = new MyPortfolio();
+
+        public ActionResult Index()
+        {
+            var degerler = _db.TblCategories.ToList();
+            
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult CreateCategory() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCategory(TblCategories categories)
+        {
+            
+            _db.TblCategories.Add(categories);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
